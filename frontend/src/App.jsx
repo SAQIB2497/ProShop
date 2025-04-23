78bb186363ff6e16c83d80f6de808fe24339a7b5
Saqib Ur Rehman <saqiburrehman75@gmail.com>
2025-04-23 21:47:22 +0500

    Adding List Product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import HomeScreen from "./screens/HomeScreen.jsx";
 
 const App = () => {
   return (
     <>
       <Header />
       <main>
-        <Container className="PY-3">
-          <h1>Welcome to ProShop</h1>
+        <Container className="Py-3">
+          <HomeScreen/>
         </Container>
         <Footer />
       </main>
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import products from "../product.js";
+import Product from "../components/product.jsx";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => {
+          return (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={4}>
+              <Product product={product} />
+            </Col>
+          );
+        })}
+      </Row>
+    </div>
+  );
+};
+
+export default HomeScreen;
