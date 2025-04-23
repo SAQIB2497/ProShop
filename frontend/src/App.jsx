494a4510422ef412e888c6bf6b3a11936799b7b8
Saqib Ur Rehman <saqiburrehman75@gmail.com>
2025-04-23 22:31:06 +0500

    Adding React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
-import HomeScreen from "./screens/HomeScreen.jsx";
-
 const App = () => {
   return (
     <>
       <Header />
       <main>
         <Container className="Py-3">
-          <HomeScreen/>
+          <Outlet />
         </Container>
         <Footer />
       </main>
