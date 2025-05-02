3366da9189af01c0e9e43ad5951738c39e14be6d
Saqib Ur Rehman <saqiburrehman75@gmail.com>
2025-05-02 18:08:39 +0500

    Adding add to cart function in CartSlice

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,12 +2,58 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = localStorage.getItem("Cart")
   ? JSON.parse(localStorage.getItem("Cart"))
-  : { cartItems: [] };
+  : {
+      cartItems: [],
+      itemsPrice: 0,
+      shippingPrice: 0,
+      taxPrice: 0,
+      totalPrice: 0,
+    };
+
+const addDecimal = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
 
 const cartSlice = createSlice({
   name: "Cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addtoCart: (state, action) => {
+      const item = action.payload;
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems.push(item);
+      }
+
+      // calculate items price
+      state.itemsPrice = addDecimal(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+
+      // calculate shipping price
+      state.shippingPrice = addDecimal(state.itemsPrice > 100 ? 0 : 10);
+
+      // calculate tax price (15%)
+      state.taxPrice = addDecimal(0.15 * state.itemsPrice);
+
+      // calculate total price
+      state.totalPrice = addDecimal(
+        Number(state.itemsPrice) +
+          Number(state.shippingPrice) +
+          Number(state.taxPrice)
+      );
+
+      // update local storage
+      localStorage.setItem("Cart", JSON.stringify(state));
+    },
+  },
 });
 
+export const { addtoCart } = cartSlice.actions;
 export default cartSlice.reducer;
+ 
